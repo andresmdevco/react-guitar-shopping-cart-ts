6b41f98831fc734ca11ac03b6732c743b2e3d4f9
Andrés Muñoz <andres2078@gmail.com>
2026-07-31 23:07:50 -0500

    refactor: define initial state for cart reducer

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,4 +1,5 @@
-import type { Guitar } from "../types";
+import { db } from "../data/db";
+import type { CartItem, Guitar } from "../types";
 
 export type CartActions = 
     | { type: 'add-to-cart', payload: {item: Guitar} } 
@@ -6,3 +7,13 @@ export type CartActions =
     | { type: 'decrease-quantity', payload: {id: Guitar['id']} }
     | { type: 'increase-quantity', payload: {id: Guitar['id']} }
     | { type: 'clear-cart' }
+
+export type CartState = {
+    data: Guitar[]
+    cart: CartItem[]
+}
+
+export const initialState: CartState = {
+    data: db,
+    cart: []
+}
